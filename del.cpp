011b317535e8fee0016d1ade8dea0e85ec380304
YarigaOleg <Oyariga@gmail.com>
2018-11-11 22:31:24 +0300

    Деление

---
+++ del.cpp
@@ -0,0 +1,11 @@
+//funcs.cpp
+#include "stdafx.h"
+#include "funcs.h"
+
+double deleting(double X, double Y){
+	if (Y!=0){
+		X/Y;
+	}else{
+		return NULL;
+	}
+}
---
+++ del.h
@@ -0,0 +1,5 @@
+// заголовочный файл funcs.h
+#ifndef __DEL__
+#define __DEL__
+double deleting(double, double);
+#endif // __DEL__
