caeac1bd472ca10b2064b6ee29116e1344596b4c
Katia <lisena.eva@yandex.ru>
2018-11-11 22:33:21 +0300

    pow

---
+++ fun_pow.cpp
@@ -0,0 +1,9 @@
+
+#include "pow.h"
+#include <cmath>
+
+
+double MyPow(double a, double b)
+{
+	return (b * a);
+}
---
+++ pow.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __FUNCS_H__ 
+#define __FUNCS_H__
+
+double MyPow(double, double);
+
+#endif // __FUNCS_H__
