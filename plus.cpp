520fdb9b1ac72398c360aa20587916ec57ddc160
Anton Ilin <anton.stu@mail.ru>
2018-11-11 22:43:50 +0300

    Plus

---
+++ plus.cpp
@@ -0,0 +1,8 @@
+#include "plus.h"
+
+
+double plus(double a, double b) {
+	double c;
+	c = a + b;
+	return c;
+}
---
+++ plus.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef __PLUS_H__
+#define __PLUS_H__
+double plus(double, double);
+#endif
