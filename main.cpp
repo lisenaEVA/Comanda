e5ea2773a6afc610fd3a2b760eee59dae762d63e
Katia <lisena.eva@yandex.ru>
2018-11-11 22:36:12 +0300

    pow1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
-int main{
-return 0;
+#include <iostream> 
+#include "pow.h"
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+	cout << MyPow(a, b) << endl;
+
+	system("pause");
+	return 0;
 }
